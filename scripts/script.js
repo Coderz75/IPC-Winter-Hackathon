89e949c3a58353ed45722c8821f4b7ce85db0fa9
Nuaym Syed <scienceboy75@gmail.com>
2024-01-11 22:39:23 -0800

    got basic details working

---
+++ scripts/script.js
@@ -0,0 +1,11 @@
+"use strict";
+
+function setup(){
+    document.getElementById("game").style.display = "none";
+    document.getElementById("info").style.display = "none";
+}
+
+function start(){
+    document.getElementById("welcome").style.display = "none";
+    document.getElementById("game").style.display = "block";
+}
